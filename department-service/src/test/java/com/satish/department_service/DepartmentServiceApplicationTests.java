8a7af191ce7c6a385d3feb1fb3ebb0af868e45c0
Satish Shingrakhiya <satish.shingrakhiy@gmail.com>
2025-01-07 21:50:31 +0530

    Create Department Service

---
+++ department-service/src/test/java/com/satish/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satish.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
